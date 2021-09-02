cca2d8fa7de6d62213627f57e9fd5627e653706a
shreetuition <info@shreetuitioncenter.com>
2021-09-02 15:20:42 +0530

    Done

---
+++ youtube.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Media in HTML</title>
+	<style type="text/css">
+		h1 {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1>Media in HTML</h1>
+	<h2>YouTube in HTML</h2>
+	<hr>
+
+	<iframe src="https://www.youtube.com/embed/?listType=playlist&list=PLEN67NEFoELvTyyl92fDJ37C7c2fYTgeB" width="500" height="300" allowfullscreen="allowfullscreen"></iframe>
+
+</body>
+</html>
