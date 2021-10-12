1e097ceb928a9ebeb589299079855b147d64fd0b
shreetuition <info@shreetuitioncenter.com>
2021-10-12 15:30:07 +0530

    Done

---
+++ JS/index.js
@@ -0,0 +1,166 @@
+
+// Types Of comments
+// 1. Single Line Comments 
+// 2. MultiLine Comments
+
+//alert("Hello This is alert Box")
+
+/*
+console.log("Hello This is Sharvan Kumar");
+
+console.log("Hello This is Sharvan Kumar");
+
+*/
+	
+
+// Here are rules JavaScript has for naming variables:
+// Variable names cannot contain spaces.
+// Variable names must begin with a letter, an underscore (_) or a dollar sign ($).
+// Variable names can only contain letters, numbers, underscores, or dollar signs.
+// Variable names are case-sensitive.
+
+// console.log("Hello This is Sharvan Kumar");
+
+// var a = 5;
+// var $name_$Of_1 = "Sharvan"
+
+// var username = "sk123"
+
+// var Username = "pk546"
+
+// console.log(a);
+
+// console.log($name_$Of_1);
+
+// console.log(Username);
+
+'use strict'
+
+// var fname = "Kumar"
+// var username = "sk124"
+// var username = "pk54654"
+// console.log(fname);
+// console.log(username)
+
+
+// camalcase
+// snaketype
+// Pascal Case
+
+
+// username  = 2
+// nameofuser = 3
+
+// userName 
+// nameOfUser 
+
+// user_name
+
+// name_of_user
+
+// NameOfUser
+
+// UserName
+
+var x = 5 // int  
+var user_name = "sk123" // string 
+var name_of_user = 'Dixit'
+var isStatus = true  // bool 
+var flotValue = 56.325 // float
+
+//var typeOf = typeof(x)
+
+
+// console.log(typeof(x))
+// console.log(typeof(user_name))
+// console.log(typeof(isStatus))
+// console.log(typeof(flotValue))
+// console.log(typeof(name_of_user));
+
+// Devin Sojitra 
+ 
+// var first_name = "Devin"
+
+// var last_name = "Sojitra"
+
+// var space = " "
+
+// console.log(first_name+last_name);
+// console.log(first_name,last_name);
+// console.log(first_name,last_name);
+// console.log(first_name+last_name);
+
+// var fullName = first_name+" "+last_name
+
+// var fullName = first_name+" "+last_name
+
+// console.log(fullName);
+
+// maths operators
+
+// +
+// -
+// *
+// /
+// %
+
+// var a = parseInt("56465")
+// var b = parseInt("6")
+
+
+// console.log(a);
+
+// console.log(typeof(a));
+
+// console.log(typeof(b));
+
+// var sub = a - b
+
+// console.log(typeof(sub));
+
+
+//console.log(a+b); // 21
+//console.log(a-b); // 9
+//console.log(a*b); // 60
+//console.log(a/b); // 2
+//console.log(a%b); // 3
+
+
+// var a,b,c;
+
+// a = b = c = 5
+
+
+// var a, b = c = 1;
+
+// var abc, bcd, casd = dasdsa = 5
+
+// console.log(a);
+
+// console.log(b);
+
+// console.log(c);
+
+
+
+var x = 5
+
+console.log(x) // 5  True => 5
+
+// console.log(x+1);
+
+console.log(x++); //  6 True => 5
+
+console.log(x); // 5 True => 6
+console.log(++x); // 6 True => 7
+
+console.log(x); // 5 True => 7
+
+
+
+
+
+
+
+
+
